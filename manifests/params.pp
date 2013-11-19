794213c4b0fda36a7d352c193403aec0b626854f
Peter Pouliot <ppouliot@gmail.com>
2013-11-19 17:33:32 -0500

    adding some logic

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,18 @@
 class windows_java::params {
   $url     = $java_url
-  $package = $java_package
+  $chocolatey = 'True',
+
+
+  $package  = $chocolatey ?{
+    'true'  => $chocolatey_package
+    default => $java_package
+  }
 
   $java_url = $::architecture ? {
     /(x64)/ => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75261',
     default => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75259',
   }
+  $chocolatey_package = 'java.jdk'
 
   $java_package = $::architecture ?{
     /(x64)/ => 'Java 7 Update 17 (64-bit)',
