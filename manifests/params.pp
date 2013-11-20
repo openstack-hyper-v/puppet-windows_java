8c4a8d2a47565fbb3f6fe4b7e606ae76fc91f4ac
Nick Meier <nmeier@microsoft.com>
2013-11-20 15:38:19 +0000

    Finishing up changes Peter started

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,22 @@
-class windows_java::params {
-  $url        = $java_url
+class windows_java::params (
+  $url        = undef,
   $chocolatey = 'true'
-
-
-  $package  = $chocolatey ?{
-    /(true)/  => $chocolatey_package,
-    default   => $java_package,
-  }
+){
 
   $java_url = $::architecture ? {
     /(x64)/ => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75261',
-    default => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75259',
+    default => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75259'
   }
-  $chocolatey_package = 'java.jdk'
 
   $java_package = $::architecture ?{
     /(x64)/ => 'Java 7 Update 17 (64-bit)',
-    default => 'Java 7 Update 17',
+    default => 'Java 7 Update 17'
   }
 
+  $chocolatey_package = 'java.jdk'
+
+  $package  = $chocolatey ?{
+    /(true)/  => $chocolatey_package,
+    default   => $java_package
+  }
 }
